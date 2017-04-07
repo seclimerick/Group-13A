a866177fa3772fb558f01e56286d8cebf41eece5
seclimerick <16195272@studentmail.ul.ie>
2017-04-07 12:38:04 +0100

    Add files via upload

--- basic proofreading/create_task.php
+++ basic proofreading/create_task.php
@@ -50,50 +50,69 @@
 										<header class="major">
 											<h2>UL Proofreading Service</h2>
 										</header>
-<?php
+<?php 
     if (isset($_POST) && count ($_POST) > 0) {
-//$user_id = htmlspecialchars(trim($_POST["user_id"]));
-$task_type = htmlspecialchars(ucfirst(trim($_POST["task_type"])));
-$title = htmlspecialchars(ucfirst(trim($_POST["title"]))); $description = htmlspecialchars(ucfirst(trim($_POST["description"])));
-$req_words = htmlspecialchars(ucfirst(trim($_POST["req_words"])));
-$word_count = htmlspecialchars(ucfirst(trim($_POST["word_count"])));
-$total_pages = htmlspecialchars(ucfirst(trim($_POST["total_pages"])));
-$file_format = htmlspecialchars(ucfirst(trim($_POST["file_format"])));
-$due_date = htmlspecialchars(ucfirst(trim($_POST["due_date"])));
-$due_time = htmlspecialchars(ucfirst(trim($_POST["due_time"])));
-$tags = htmlspecialchars(ucfirst(trim($_POST["tags"])));
+$Student_ID = "12345678"; 
+//$_SESSION["user_id"];
+$Task_Type = htmlspecialchars(ucfirst(trim($_POST["Task_Type"])));
+$Title = htmlspecialchars(ucfirst(trim($_POST["Title"]))); 
+$Description = htmlspecialchars(ucfirst(trim($_POST["Description"])));
+$Req_Words = htmlspecialchars(ucfirst(trim($_POST["Req_Words"])));
+$Word_Count = htmlspecialchars(ucfirst(trim($_POST["Word_Count"])));
+$Total_Pages = htmlspecialchars(ucfirst(trim($_POST["Total_Pages"])));
+$File_Format = htmlspecialchars(ucfirst(trim($_POST["File_Format"])));
+$Due_Date = htmlspecialchars(ucfirst(trim($_POST["Due_Date"])));
+//printf("Due Date: %s ",$Due_Date);
+$Due_Time = htmlspecialchars(ucfirst(trim($_POST["Due_Time"])));
+//$Due_Time = $Due_Time.":00";
+//printf("Due Time: %s ",$Due_Time);
+$Tag = htmlspecialchars(ucfirst(trim($_POST["Tag"])));
 //$passOne = $_POST["pass_one"]; $passTwo = $_POST["pass_two"];
 //check wheter user/email alerady exists
-/*$dbh = new PDO("mysql:host=localhost;dbname=group13", "root");
-$stmt = $dbh->prepare("SELECT user_id, email, password1 FROM userinfo WHERE email = ?" );
-$stmt->execute(array($email));
+$dbh = new PDO("mysql:host=localhost;dbname=group13", "root", "");
+/*$stmt = $dbh->prepare("SELECT task_id FROM tasks WHERE task_id = ?" );
+$stmt->execute(array($task_id));
 $rowCount = $stmt->rowCount();
 if ($passOne != $passTwo) { //in case Javascript is disabled.
-printf("<h2> Passwords do not match. </h2>");
+	printf("<h2> Passwords do not match. </h2>");
 } else {
-if ($rowCount > 0) {
-printf("<h2> An account already exists with the given email.</h2>");
-} else {*/
-$query = "INSERT INTO tasks SET task_id = :task_id, user_id = :user_id, date = :date, expiry_date = :expiry_date, task_type = :task_type, title = :title, req_words = :req_words, word_count = :word_count, total_pages = :total_pages, file_format = :file_format, due_date = :due_date, due_time = :due_time, description = :description, tag = :tag";
-$stmt = $dbh->prepare($query);
-$siteSalt = "group13";
-//$affectedRows = $stmt->execute(array(':user_id' => $user_id, ':user_type' => $user_type, ':email' => $email, ':first_name' => $firstName, ':last_name' => $lastName, ':password' => $saltedHash, ':level' => $level, ':discipline' => $discipline, ':current' => $current));
-//if ($affectedRows > 0) {
-//$insertId = $dbh->lastInsertId();
-printf("<h2> Task successfully created %s! Please <a href=\"./login.php\"> login </a> to proceed. </h2>", $firstName);
-//logout first
-/*http://php.net/manual/en/function.session-unset.php*/
-session_unset(); session_destroy(); session_write_close(); setcookie(session_name(),'',0,'/');
-session_regenerate_id(true);
-} } } }
+			if ($rowCount > 0) {
+			printf("<h2> This task already exists.</h2>");
+			} else {*/
+			
+			//$student_id = 15181596
+				$query = "INSERT INTO `tasks` (`Task_ID`,`Task_Type`, `Title`, `Req_Words`, `Word_Count`, `Total_Pages`, `File_Format`, `Due_Date`, `Due_Time`, `Description`, `Tag`, `Student_ID`) VALUES (null, :Task_Type , :Title, :Req_Words, :Word_Count, :Total_Pages, :File_Format, :Due_Date, :Due_Time, :Description, :Tag, null)";
+				//$query = "INSERT INTO Tasks SET Student_ID = :Student_ID, Task_Type = :Task_Type, Title = :Title, Req_Words = :Req_Words, Word_Count = :Word_Count, Total_Pages = :Total_Pages, File_Format = :File_Format, Due_Date = :Due_Date, Due_Time = :Due_Time, Description = :Description, Tags = :Tags";
+				//$stmt->bindValue(':Task_Type', $Task_Type);
+				$stmt = $dbh->prepare($query);
+				//$siteSalt = "group13";
+				$temp = array(':Task_Type' => $Task_Type, ':Title' => $Title, ':Req_Words' => $Req_Words, ':Word_Count' => $Word_Count, ':Total_Pages' => $Total_Pages, ':File_Format' => $File_Format, ':Due_Date' => $Due_Date, ':Due_Time' => $Due_Time, ':Description' => $Description, ':Tag' => $Tag);
+				
+				print_r($temp); 
+				$affectedRows = $stmt->execute($temp);
+				$insertId = $dbh->lastInsertId();
+				printf("<h2> Task successfully created! </h2>".$affectedRows."--".$insertId); 
+				if ($affectedRows > 0) {
+					printf("Inserted rows is equal to %s", $affectedRows);
+					$insertId = $dbh->lastInsertId();
+					printf("<h2> Task successfully created!");
+					//logout first
+					//http://php.net/manual/en/function.session-unset.php
+					session_unset(); session_destroy(); session_write_close(); setcookie(session_name(),'',0,'/');
+					session_regenerate_id(true);
+				}
+			
+		//}			
+}
 
 ?>
 <?php 
 
-if (!isset($_POST) || count($_POST) == 0) { ?>
-											<form method="post">
+if (!isset($_POST) || count($_POST) == 0) { 
+?> 
+		<form method="post">
 												<label> Task Type:</label>
-												<select name = "task_type">
+												<select name = "Task_Type">
 												<option value = '-Pick From List-'>-Pick From List-</option>
 												<option value = 'Assignment'>Assignment</option>
 												<option value = 'Dissertation'>Dissertation</option>
@@ -102,17 +121,17 @@ if (!isset($_POST) || count($_POST) == 0) { ?>
 												</select>
 												<br>
 												<label> Title:</label>
-												<input type="text" name="title" placeholder="title" required="required" maxlength="255"/>
+												<input type="text" name="Title" placeholder="Title" required="required" maxlength="255"/>
 												<label> Description:</label>
-												<input type="text" name="description" placeholder="description" maxlength="60"/>
+												<input type="text" name="Description" placeholder="Description" maxlength="60"/>
 												<label> Required Words:</label>
-												<input type="number" name="required_words" placeholder="required_words" maxlength="60"/>
+												<input type="number" name="Req_Words" placeholder="Required_Words" maxlength="60"/>
 												<label> Word Count:</label>
-												<input type="number" name="word_count" placeholder="word_count" maxlength="60"/>
+												<input type="number" name="Word_Count" placeholder="Word_Count" maxlength="60"/>
 												<label> Total Pages:</label>
-												<input type="number" name="total_pages" placeholder="total_pages" maxlength="60"/>
+												<input type="number" name="Total_Pages" placeholder="Total_Pages" maxlength="60"/>
 												<label> File Format:</label>
-												<select name = "file_format">
+												<select name = "File_Format">
 												<option value = '-Pick From List-'>-Pick From List-</option>
 												<option value = 'doc'>.doc</option>
 												<option value = 'docx'>.docx</option>
@@ -122,36 +141,36 @@ if (!isset($_POST) || count($_POST) == 0) { ?>
 												<br>
 												<ul>
 												<label> Due Date:</label>
-												<input type="date" name="due_date" placeholder="due_date" required="required"/>
+												<input type="date" name="Due_Date" placeholder="Due_Date" required="required"/>
 												<label>Due Time:</label>
-												<input type="time" name="due_time" placeholder="due_time" required="required"/>
+												<input type="time" name="Due_Time" placeholder="Due_Time" required="required"/>
 												<label> Tags:</label>
 												<form> <p>Pick 4 tags maximum </p> 
 												<script type="text/javascript">
 												</script>
 												</head> 
 												<body> 
-												<div id="ScrollCB" style="height:150;width:400px;overflow:auto" name = tags> 
-												<input type="checkbox" id="Accounting" value="Accounting"><label for="Accounting"> Accounting</label><br>
-												<input type="checkbox" id="Database Design" value="Database Design"><label for="Database Design"> Database Design</label><br>
-												<input type="checkbox" id="Design" value="Design"><label for="Design"> Design</label><br>
-												<input type="checkbox" id="Disabilities" value="Disabilities"><label for="Disabilities"> Disabilities</label><br>
-												<input type="checkbox" id="Education" value="Education"><label for="Education"> Education</label><br>
-												<input type="checkbox" id="Engineering" value="Engineering"><label for="Engineering"> Engineering</label><br>
-												<input type="checkbox" id="Equine" value="Equine"><label for="Equine"> Equine</label><br>
-												<input type="checkbox" id="Food Sciences" value="Food Sciences"><label for="Food Sciences"> Food Sciences</label><br>
-												<input type="checkbox" id="Health Sciences" value="Health Sciences"><label for="Health Sciences"> Health Sciences</label><br>
-												<input type="checkbox" id="Law" value="Law"><label for="Law"> Law</label><br>
-												<input type="checkbox" id="Medical" value="Medical"><label for="Medical"> Medical</label><br>
-												<input type="checkbox" id="Mobile App Design" value="Mobile App Design"><label for="Mobile App Design"> Mobile App Design</label><br>
-												<input type="checkbox" id="Networking" value="Networking"><label for="Networking"> Networking</label><br>
-												<input type="checkbox" id="Nursing" value="Nursing"><label for="Nursing"> Nursing</label><br>
-												<input type="checkbox" id="Programming" value="Programming"><label for="Programming"> Programming</label><br>
-												<input type="checkbox" id="Psychology" value="Psychology"><label for="Psychology"> Psychology</label><br>
-												<input type="checkbox" id="Software" value="Software"><label for="Software"> Software</label><br>
-												<input type="checkbox" id="Sport" value="Sport"><label for="Sport"> Sport</label><br>
-												<input type="checkbox" id="Technology" value="Technology"><label for="Technology"> Technology</label><br>
-												<input type="checkbox" id="Web Design" value="Web Design"><label for="Web Design"> Web Design</label><br>
+												<div id="ScrollCB" style="height:150;width:400px;overflow:auto" name = Tag> 
+												<input type="checkbox" id="Accounting" value="Accounting" name = Tag><label for="Accounting"> Accounting</label><br>
+												<input type="checkbox" id="Database Design" value="Database Design" name = Tag><label for="Database Design"> Database Design</label><br>
+												<input type="checkbox" id="Design" value="Design"><label for="Design" name = Tag> Design</label><br>
+												<input type="checkbox" id="Disabilities" value="Disabilities" name = Tag><label for="Disabilities"> Disabilities</label><br>
+												<input type="checkbox" id="Education" value="Education" name = Tag><label for="Education"> Education</label><br>
+												<input type="checkbox" id="Engineering" value="Engineering" name = Tag><label for="Engineering"> Engineering</label><br>
+												<input type="checkbox" id="Equine" value="Equine"><label for="Equine" name = Tags> Equine</label><br>
+												<input type="checkbox" id="Food Sciences" value="Food Sciences" name = Tag><label for="Food Sciences"> Food Sciences</label><br>
+												<input type="checkbox" id="Health Sciences" value="Health Sciences" name = Tag><label for="Health Sciences"> Health Sciences</label><br>
+												<input type="checkbox" id="Law" value="Law"><label for="Law" name = Tag> Law</label><br>
+												<input type="checkbox" id="Medical" value="Medical"><label for="Medical" name = Tag> Medical</label><br>
+												<input type="checkbox" id="Mobile App Design" value="Mobile App Design" name = Tag><label for="Mobile App Design"> Mobile App Design</label><br>
+												<input type="checkbox" id="Networking" value="Networking" name = Tag><label for="Networking"> Networking</label><br>
+												<input type="checkbox" id="Nursing" value="Nursing" name = Tag><label for="Nursing"> Nursing</label><br>
+												<input type="checkbox" id="Programming" value="Programming" name = Tag><label for="Programming"> Programming</label><br>
+												<input type="checkbox" id="Psychology" value="Psychology" name = Tag><label for="Psychology"> Psychology</label><br>
+												<input type="checkbox" id="Software" value="Software" name = Tag><label for="Software"> Software</label><br>
+												<input type="checkbox" id="Sport" value="Sport" name = Tag><label for="Sport"> Sport</label><br>
+												<input type="checkbox" id="Technology" value="Technology" name = Tag><label for="Technology"> Technology</label><br>
+												<input type="checkbox" id="Web Design" value="Web Design" name = Tag><label for="Web Design"> Web Design</label><br>
 												</div>
 											
 												
@@ -159,17 +178,22 @@ if (!isset($_POST) || count($_POST) == 0) { ?>
 												<button type="reset" class="button small">Reset</button>
 												
 										</form>
-<?php } ?>									
+										<?php 
+												}
+										?>
+										
+									
 												<ul class="actions small">
-												<?php
+										<?php 	
 												if (!isset ($_SESSION)) {
 												session_start();
 												}
 							
 												if (isset($_SESSION["user_id"]) && $_SESSION["user_id"] != ''){ 
-												?>
-													<li><a href="#" class="button special small">Commit to buy</a></li>
-												<?php } ?>
+												
+													printf("<li><a href=\"#\" class=\"button special small\">Commit to buy</a></li>");
+												} 
+										?>
 													<li><a href="./index.php" class="button small">Back</a></li>
 												</ul>
 
